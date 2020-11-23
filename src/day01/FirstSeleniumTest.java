00d2ded562c27a9a78c4b7e6713c6134c22cbd7e
Ebubekir <esahin3@icloud.com>
2020-11-23 23:52:38 +0300

    First Commit

---
+++ src/day01/FirstSeleniumTest.java
@@ -0,0 +1,15 @@
+package day01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class FirstSeleniumTest {
+    public static void main(String[]args){
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\ASUS\\Documents\\selenium dependencies\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+
+    }
+}
